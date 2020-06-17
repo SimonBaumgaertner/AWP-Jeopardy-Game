3fb3a9c9482073cf90f80401db0d5d302aa9dc83
Marcel Anton <marcel.anton@ssi-schaefer.com>
2020-06-17 11:47:41 +0200

    Create MainView from the Game

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-
+private double x,y;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 350));
+        Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getSceneX() - x);
+            primaryStage.setY(event.getSceneY() -y);
+        });
         primaryStage.show();
     }
 
