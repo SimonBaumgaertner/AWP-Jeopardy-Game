3214e540084844baa9d8c47227174c55b05fc9a4
Marcel Anton <marcel.anton@ssi-schaefer.com>
2020-06-19 12:28:25 +0200

    add question window and some function to open it

--- src/sample/GameView.java
+++ src/sample/GameView.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,8 +20,11 @@ public class GameView implements Initializable {
 
     @FXML
     public Label Count;
-    public Button d400;
     public Label nameFieldGame;
+    public Button k1d400;
+    public Button k2d400;
+    public Button k3d400;
+    public Button k4d400;
 
     @FXML
     public void closeGame(ActionEvent actionEvent)throws IOException {
@@ -32,19 +36,42 @@ public class GameView implements Initializable {
         window.setScene(scene);
         window.show();
     }
-
-    public void d400Buttons(ActionEvent actionEvent)throws IOException {
-
+    // TODO this function should be in QuestionView
+    private void disableButtonAndCount(Button button, int value){
         int points = Integer.valueOf(Count.getText());
-        points = points + 400;
+        points = points + value;
         Count.setText(String.valueOf(points));
-        d400.setDisable(true);
+        button.setDisable(true);
+    }
 
+    private void showQuestion(String category, int value) throws IOException {
 
+        Parent questionView = FXMLLoader.load(getClass().getResource("questionView.fxml"));
+
+        Scene scene = new Scene(questionView);
+        Stage window = new Stage();
+        window.setScene(scene);
+        window.initStyle(StageStyle.UNDECORATED);
+        window.show();
+
+        //TODO get from the database the designated questions in this category and with this value
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void d400k1button(ActionEvent actionEvent)throws IOException {
+        showQuestion("K1",400);
+    }
+
+    public void d400k2button(ActionEvent actionEvent) {
+    }
+
+    public void d400k3button(ActionEvent actionEvent) {
+    }
+
+    public void d400k4button(ActionEvent actionEvent) {
     }
 }
